733e4e77c7a10a084b78ca688d88a80b5857bbc2
iamprosper <thinkpadprosper@gmail.com>
2022-03-10 18:21:11 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Printing a string with puts function
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
