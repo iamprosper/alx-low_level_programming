3b6aaffc64f41110a1be4954686f46fb25ea8e68
iamprosper <thinkpadprosper@gmail.com>
2022-03-18 02:51:42 +0000

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Multiplying two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: the multiplication result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
