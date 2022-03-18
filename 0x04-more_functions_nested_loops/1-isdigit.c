b4b331db6fe756d169801609051fbbe9983f9059
iamprosper <thinkpadprosper@gmail.com>
2022-03-18 02:42:51 +0000

    Digit checking

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - check digits number.
+ * 
+ * Return: 1 in case of succes, 0 in case of failure.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
