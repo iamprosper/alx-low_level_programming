8e9149ed092f08e39364be8ee78f2f50fe34b53f
iamprosper <thinkpadprosper@gmail.com>
2022-03-18 03:53:28 +0000

    Printintg diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - Print \ n times
+ * @n: int
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
