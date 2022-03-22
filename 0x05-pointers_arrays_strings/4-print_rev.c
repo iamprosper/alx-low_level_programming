554d031636119041bea909123ed9241ff686ad83
iamprosper <thinkpadprosper@gmail.com>
2022-03-22 21:34:13 +0000

    Printing a reversed string pointed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: Pointer to char
+ *
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len > 0)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
+	_putchar('\n');
+}
