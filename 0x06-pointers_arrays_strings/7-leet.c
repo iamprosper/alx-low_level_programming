4c7ac1853cc26b0aec8d828bb8f0eeed67f6b4e3
iamprosper <thinkpadprosper@gmail.com>
2022-03-24 21:50:16 +0000

    Coding char

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - String encoding
+ * @c: Pointer to char
+ *
+ * Return: A pointer to char
+ */
+char *leet(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] == 97 || c[i] == 65)
+			c[i] = 52;
+		else if (c[i] == 101 || c[i] == 69)
+			c[i] = 51;
+		else if (c[i] == 111 || c[i] == 79)
+			c[i] = 48;
+		else if (c[i] == 116 || c[i] == 84)
+			c[i] = 55;
+		else if (c[i] == 108 || c[i] == 76)
+			c[i] = 49;
+		i++;
+	}
+
+	return (c);
+}
