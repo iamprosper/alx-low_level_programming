260711a356c9fe23e4fdfd124441ef2700253a66
iamprosper <thinkpadprosper@gmail.com>
2022-03-24 22:18:05 +0000

    Check#2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,19 +8,32 @@
 char *leet(char *c)
 {
 	int i = 0;
+	int j;
+
+	char letters[] = "aeotl";
+	char numbers[] = "43071";
 
 	while (c[i] != '\0')
 	{
-		if (c[i] == 97 || c[i] == 65)
-			c[i] = 52;
-		else if (c[i] == 101 || c[i] == 69)
-			c[i] = 51;
-		else if (c[i] == 111 || c[i] == 79)
-			c[i] = 48;
-		else if (c[i] == 116 || c[i] == 84)
-			c[i] = 55;
-		else if (c[i] == 108 || c[i] == 76)
-			c[i] = 49;
+		j = 0;
+		while (letters[j] != '\0')
+		{
+			if ((c[i] == letters[j]) || (c[i] == letters[j] - 32))
+				c[i] = numbers[j];
+			j++;
+		}
+		/*
+		 * if (c[i] == 97 || c[i] == 65)
+		*	c[i] = 52;
+		*else if (c[i] == 101 || c[i] == 69)
+		*	c[i] = 51;
+		*else if (c[i] == 111 || c[i] == 79)
+		*	c[i] = 48;
+		*else if (c[i] == 116 || c[i] == 84)
+		*	c[i] = 55;
+		*else if (c[i] == 108 || c[i] == 76)
+		*	c[i] = 49;
+		*/
 		i++;
 	}
 
