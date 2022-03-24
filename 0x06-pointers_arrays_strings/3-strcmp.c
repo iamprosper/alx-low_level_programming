020069b2dd300739bc99522b1747b218545d60b1
iamprosper <thinkpadprosper@gmail.com>
2022-03-24 04:03:55 +0000

    Check#1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1_len = 0;
-	int s2_len = 0;
+	int i;
 
-	while (s1[s1_len] != '\0')
-		s1_len++;
-
-	while (s2[s2_len] != '\0')
-		s2_len++;
-
-	if (s1_len < s2_len)
-		return (-1);
-	else if (s1_len > s2_len)
-		return (1);
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 
 	return (0);
 }
