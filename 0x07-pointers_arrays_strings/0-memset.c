b73a5abae488f3aeaa9a362aae2fe41b4f03f8c0
iamprosper <thinkpadprosper@gmail.com>
2022-03-28 22:20:19 +0000

    More pointers manipulations

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _memset - Fill memory with a constante byte
+* @s: A pointer to a char
+* @b: A char
+* @n: An integer
+*
+* Return: A pointer to a char
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for(i = 0; i < n; i++)
+		*(s + i) = b;
+	return  s;
+}
