7961e5365f91df9d9698cfd8cd2db644e397afce
iamprosper <thinkpadprosper@gmail.com>
2022-03-30 19:08:10 +0000

    Checked

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 void print_char(char *s);
+int move_next(char *s, int i);
 /**
  * _print_rev_recursion - Prints a string in reverse
  * @s: A pointer to a char variable
@@ -8,11 +9,8 @@ void print_char(char *s);
 void _print_rev_recursion(char *s)
 {
 	int s_len = 0;
-	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-		s_len++;
-
+	s_len = move_next(s, s_len);
 	print_char(&(*(s + s_len - 1)));
 }
 
@@ -27,4 +25,21 @@ void print_char(char *s)
 		_putchar(*(s + 0));
 		print_char(&(*(s - 1)));
 	}
+}
+
+/**
+ * move_next - Move to the next char
+ * @s: A pointer to char
+ * @i: An integer
+ *
+ * Return: an integer acting like index
+ */
+int move_next(char *s, int i)
+{
+	if (*(s + 1) != '\0')
+	{
+		i++;
+		return (move_next(&(*(s + 1)), i));
+	}
+	return (i);
 }
