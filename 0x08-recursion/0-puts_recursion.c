dae4c6a00ae29d058ef186351d42538a3a04e356
iamprosper <thinkpadprosper@gmail.com>
2022-03-30 15:36:24 +0000

    Correcting null string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,9 +5,12 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*(s));
-	if (*(s + 1) != '\0')
+	if(*(s + 0) != '\0')
+	{
+		_putchar(*(s));
 		_puts_recursion(&(*(s + 1)));
+	}
 	else
 		_putchar('\n');
+
 }
