5b79cd4f40e5a6e5eea6159d91d6ad516e3ef044
iamprosper <thinkpadprosper@gmail.com>
2022-03-30 21:47:30 +0000

    Printing the lenght of a string with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+int _recursion(char *s, int i);
+/**
+ * _strlen_recursion - Get the lenght of a string
+ * @s: A pointer to char
+ *
+ * Return: An integer, the lenght of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	return (_recursion(s, i));
+}
+
+/**
+ * _recursion - Move to the next character
+ * @s: A char pointer
+ * @i: An integer
+ *
+ * Return: An integer, the lenght of the string
+ */
+int _recursion(char *s, int i)
+{
+	if (*s != '\0')
+	{
+		i++;
+		return (_recursion(&(*(s + 1)), i));
+	}
+	return (i);
+}
