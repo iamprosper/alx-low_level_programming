1b7fcd0dbd8bb89b8feb4336b731b9c4c775f474
iamprosper <thinkpadprosper@gmail.com>
2022-04-01 08:20:27 +0000

    Print arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Main block
+ * @argc: An integer, the number of command line arguments
+ * @argv: An array of pointers
+ *
+ * Return: 0 in case of success, 1 in case of failure
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	exit(EXIT_SUCCESS);
+}
