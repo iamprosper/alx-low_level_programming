6d09de0a20db5d5efca2cd5f05a3b6e12115a97c
iamprosper <thinkpadprosper@gmail.com>
2022-04-01 13:13:06 +0000

    Checked#2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
  *
  * Return: 0 in case of success, 1 in case of failure
  */
-int main(int argc, char **argv)
+int main(int argc, char __attribute__((__unused__)) **argv)
 {
 	printf("%d\n", argc - 1);
 	exit(EXIT_SUCCESS);
