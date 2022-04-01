f3da52d0a474f05c5ad6926f146ad1965ffccbb5
iamprosper <thinkpadprosper@gmail.com>
2022-04-01 07:55:28 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - The main program
+ * @argc: An integer variable, the number of command line arguments
+ * @argv: An array of pointers
+ *
+ * Return: 0 in case of success, 1 in case of failure
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
+}
