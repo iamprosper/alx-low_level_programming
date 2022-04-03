4d53fc8ac1ff0b0428af925b4c23dd7c2e160640
iamprosper <thinkpadprosper@gmail.com>
2022-04-03 11:00:45 +0000

    Correct number checked

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,20 @@ int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
-		printf("%d\n", (**(argv + 1) - '0') * (**(argv + 2) - '0'));
+		int ft_nb;
+		int sc_nb;
+		long result;
+
+		ft_nb = strtol(argv[1], NULL, 10);
+		sc_nb = strtol(argv[2], NULL, 10);
+
+		result = ft_nb * sc_nb;
+		printf("%ld\n", result);
+		/*printf("%ld\n", (**(argv + 1) - '0') * (**(argv + 2) - '0'));*/
 		exit(EXIT_SUCCESS);
 	}
 	printf("Error\n");
 	exit(EXIT_FAILURE);
 }
+
+
