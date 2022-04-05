727dd19b01956f91b21e41685f4ecfce43269fdb
iamprosper <thinkpadprosper@gmail.com>
2022-04-05 18:58:17 +0000

    Checked#1

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,10 @@ int **alloc_grid(int width, int height)
 	grid = (int **)malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 
 
 	for (i = 0; i < height; i++)
@@ -28,7 +31,12 @@ int **alloc_grid(int width, int height)
 		grid[i] = (int *)malloc(sizeof(int) * width);
 
 		if (grid[i] == NULL)
+		{
+			while (--i >= 0)
+				free(grid[i]);
+			free(grid);
 			return (NULL);
+		}
 
 		for (j = 0; j < width; j++)
 			grid[i][j] = value;
