865b04ff16258d03172dad6e0cc7ef2dd2f6433d
iamprosper <thinkpadprosper@gmail.com>
2022-04-09 03:17:17 +0000

    Checked#5

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,66 +18,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int j = 0;
 	char *new_str;
 
-	if (s1 != NULL && s2 != NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	if (n > len_str(s2))
+		src_len = len_str(s2);
+	else if (n < len_str(s2))
+		src_len = n;
+
+	dest_len = len_str(s1);
+	new_str_len = dest_len + src_len;
+	new_str = (char *) malloc(sizeof(char) * (new_str_len + 1));
+
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0, j = 0; i < new_str_len; i++)
 	{
-		if (n > len_str(s2))
-			src_len = len_str(s2);
-		else if (n < len_str(s2))
-			src_len = n;
-		dest_len = len_str(s1);
-		new_str_len = dest_len + src_len;
-		new_str = (char *) malloc(sizeof(char) * (new_str_len + 1));
-
-		if (new_str == NULL)
-			return (NULL);
-
-		while (i < dest_len)
-		{
+		if (i < dest_len)
 			new_str[i] = s1[i];
-			i++;
-		}
-		while (j < src_len)
-		{
-			new_str[i] = s2[j];
-			j++;
-			i++;
-		}
-	}
-	else if (s2 == NULL && s1 != NULL)
-	{
-		new_str = malloc(sizeof(char) * (len_str(s1) + 1));
-
-		if (new_str == NULL)
-			return (NULL);
-
-		while (s1[i] != '\0')
-		{
-			new_str[i] = s1[i];
-			i++;
-		}
-	}
-	else if (s1 == NULL && s2 != NULL)
-	{
-		if (n == 0)
-			return (malloc(sizeof(char)));
-		if (n >= len_str(s2))
-			src_len = len_str(s2);
 		else
-			src_len = n;
-
-		new_str = malloc(sizeof(char) * (src_len + 1));
-
-		if (new_str == NULL)
-			return (NULL);
-
-		while (i < src_len)
-		{
-			new_str[i] = s2[i];
-			i++;
-		}
+			new_str[i] = s2[j++];
 	}
-	else
-		new_str = malloc(sizeof(char));
+
 	new_str[i] = '\0';
 
 	return (new_str);
