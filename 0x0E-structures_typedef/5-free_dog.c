2a3e66e11827e19cd5b3ce7877aeef89f3709457
iamprosper <thinkpadprosper@gmail.com>
2022-04-12 02:25:06 +0000

    Last tasks

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - A function that frees dogs
+ * @d: A pointer to a the dog structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+		EXIT_SUCCESS;
+	}
+	EXIT_FAILURE;
+}
