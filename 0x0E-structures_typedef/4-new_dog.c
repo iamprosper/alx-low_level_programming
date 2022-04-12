5f0c3bb4d00dffcd20ee97cf16cd661d625b443a
iamprosper <thinkpadprosper@gmail.com>
2022-04-12 02:36:06 +0000

    Checked#1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * new_dog - A function that create a new dog
  * @name: The name of the dog
@@ -14,38 +13,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	char *dog_name;
 	char *owner_name;
-	int j = 0;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 	if (name != NULL && owner != NULL)
 	{
-		dog_name = malloc(sizeof(char) * (str_len(name) + 1));
-		owner_name = malloc(sizeof(char) * (str_len(owner) + 1));
-		if (dog_name == NULL || owner_name == NULL)
-			return (NULL);
-
-		while (name[j] != '\0')
-		{
-			dog_name[j] = name[j];
-			j++;
-		}
-		dog_name[j] = '\0';
-		printf("%s\n", dog_name);
-
-		for (j = 0; owner[j] != '\0'; j++)
-			owner_name[j] = owner[j];
-		owner_name[j] = '\0';
-
+		/*dog_name = malloc(sizeof(char) * (str_len(name) + 1));
+		*owner_name = malloc(sizeof(char) * (str_len(owner) + 1));
+		*if (dog_name == NULL || owner_name == NULL)
+		*	return (NULL);
+		*/
+		dog_name = name;
+		owner_name = owner;
 		(*new_dog).name = dog_name;
 		new_dog->owner = owner_name;
 		new_dog->age = age;
-		if (dog_name != NULL && owner_name != NULL)
-		{
-			free(dog_name);
-			free(owner_name);
-		}
+
 		return (new_dog);
 	}
 	return (NULL);
