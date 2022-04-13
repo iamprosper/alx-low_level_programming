b0bc1b25aa2e824d0c678c142feef4945fe7dcea
iamprosper <thinkpadprosper@gmail.com>
2022-04-13 02:23:27 +0000

    Task 4, what is ur problem?

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,41 +11,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = NULL;
-	int i = 0;
-	/*char *dog_name;*/
-	/*char *owner_name;*/
+	char *dog_name, *owner_name;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog->name = malloc(sizeof(char) * (str_len(name) + 1));
-	if (new_dog->name == NULL)
+	dog_name = malloc(sizeof(char) * (str_len(name) + 1));
+	if (dog_name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	new_dog->owner = malloc(sizeof(char) * (str_len(owner) + 1));
-	if (new_dog->owner == NULL)
+	owner_name = malloc(sizeof(char) * (str_len(owner) + 1));
+	if (owner_name == NULL)
 	{
-		free(new_dog->name);
+		free(dog_name);
 		free(new_dog);
 		return (NULL);
 	}
 
-	for (i = 0; name[i] != '\0'; i++)
-		new_dog->name[i] = name[i];
-
-	for (i = 0; owner[i] != '\0'; i++)
-		new_dog->owner[i] = owner[i];
-
-	/*new_dog->name = populate(name, new_dog->name);*/
-	/*new_dog->owner = populate(owner, new_dog->owner);*/
-	/*new_dog->name = dog_name;*/
+	dog_name = populate(name, dog_name);
+	owner_name = populate(owner, owner_name);
+	new_dog->name = dog_name;
 	/*printf("new - dog: %i\n", new_dog->name == dog_name);*/
 	/*printf("dog - name: %i\n", dog_name == name);*/
-	/*new_dog->owner = owner_name;*/
+	new_dog->owner = owner_name;
 	new_dog->age = age;
 
 	return (new_dog);
