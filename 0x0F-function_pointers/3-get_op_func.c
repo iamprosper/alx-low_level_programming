efe243d81b75af6fbd2c65c9cb4e4ec4b50f2935
iamprosper <thinkpadprosper@gmail.com>
2022-04-13 19:42:10 +0000

    Task 3 checked

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+/**
+ * get_op_func - A function that check the correct operator
+ *
+ * @s: A function pointer
+ *
+ * Return: A pointer to a function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (*ops[i].op != '\0' && strlen(s) == 1)
+	{
+		/*printf("Inside\n");*/
+		/*printf("s is %c - ",*s);*/
+		/*printf("*ops[i].op is %c\n", *ops[i].op);*/
+		/*printf("%i\n", *s == *ops[i].op);*/
+		/*
+		 * printf("Checking *s++ status: %i -
+		 * Size of s: %lu\n", *s++ == '\0', sizeof(s));
+		 */
+		/*printf("*s is: %c - *s++: %c\n",*s, *s++);*/
+		if (*s == *ops[i].op)
+		{
+			/*printf("Checked success\n");*/
+			return (ops[i].f);
+		}
+		/*printf("Checking the next op.....\n");*/
+		i++;
+	}
+	/*printf("Not correspondind\n");*/
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+/**
+ * main - Program entry point
+ * @argc: The number of arguments
+ * @argv: A pointer to the array of arguments in string
+ *
+ * Return: 98, 99 100 depending on the errors encountered,  1 in cas of success
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2, result;
+	int (*get_op)(int, int) = NULL;
+
+	if (argc == 4)
+	{
+		/*printf("Arg 1: %s\n", argv[1]);*/
+		/*printf("Arg 2: %s\n", argv[2]);*/
+		/*printf("Arg 3: %s\n", argv[3]);*/
+		n1 = _atoi(argv[1]);
+		n2 = _atoi(argv[3]);
+
+		if ((argv[2][0] == '/' || argv[2][0] == '%')
+				&& n2 == 0
+				&& strlen(argv[2]) == 1)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+
+		get_op = get_op_func(argv[2]);
+		if (get_op == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+
+		result = get_op(n1, n2);
+		printf("%d\n", result);
+		return (EXIT_SUCCESS);
+	}
+	printf("Error\n");
+	exit(98);
+}
