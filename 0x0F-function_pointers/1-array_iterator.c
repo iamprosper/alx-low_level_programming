18bbc2b66651d40b8321c49bebee6ab4c1af03a1
iamprosper <thinkpadprosper@gmail.com>
2022-04-13 04:33:28 +0000

    Task 1: Executing function on each element of array with function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Execute a function on each element of an array
+ * @array: The array of int
+ * @size: The size of the array
+ * @action: The function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
