c033a64369a36ffc1b077da14e97b045f2e8fa70
iamprosper <thinkpadprosper@gmail.com>
2022-04-13 22:25:37 +0000

    Correct checked function name

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+/**
+ * op_add - Get the sum of a and b
+ *
+ * @a: An integer
+ * @b: An integer
+ *
+ * Return: The sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Get the difference of a and b
+ *
+ * @a: An integer
+ * @b: An inteher
+ *
+ * Return: The difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Get the product of a and b
+ *
+ * @a: An integer
+ * @b: An inteher
+ *
+ * Return: The result of the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Get the division of a and b
+ *
+ * @a: An integer
+ * @b: An inteher
+ *
+ * Return: The result of the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Get the reminder of the division of a and b
+ *
+ * @a: An integer
+ * @b: An inteher
+ *
+ * Return: The modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
