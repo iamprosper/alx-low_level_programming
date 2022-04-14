036246f3ee4f4ad102643d40005c2d71081bb3f0
iamprosper <thinkpadprosper@gmail.com>
2022-04-14 15:56:30 +0000

    Task 2 checked

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	while (i < size)
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
-		i++;
-	}
+	if (array != NULL && cmp != NULL)
+		while (i < size)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+			i++;
+		}
 	return (-1);
 }
