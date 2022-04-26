852eff8dabd67729df99ba0147e355a8df81f253
iamprosper <thinkpadprosper@gmail.com>
2022-04-26 05:56:41 +0000

    Checkint Task 9#2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node = NULL;
 	unsigned int counter = 0;
 
-	if (head != NULL)
+	if (head != NULL && *head != NULL)
 	{
 		new_node = check_node_to_insert(*head, idx, n, counter);
 	}
