7090b0d0c6433258f4c8bd07f49ff7fb95a43f4f
iamprosper <thinkpadprosper@gmail.com>
2022-04-27 04:24:20 +0000

    Checking task 9_rev#1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,10 +16,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		head = malloc(sizeof(listint_t *));
 		head[0] = malloc(sizeof(listint_t));
+		if (head == NULL || *head == NULL)
+			return (NULL);
 	}
 	if (idx == counter)
 	{
 		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
 		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
