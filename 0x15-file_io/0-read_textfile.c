8efa895d83df3e1fc6090608910f816d75f382f8
iamprosper <thinkpadprosper@gmail.com>
2022-05-03 04:58:58 +0000

    Opening file and reading it's content

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * read_textfile - Read a text file
+ * @filename: The string to print
+ * @letters: The number of letters to print
+ *
+ * Return: The number of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf = malloc(sizeof(char) * letters);
+	ssize_t nb = 0;
+	int i = -1;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd != -1)
+	{
+		read(fd, buf, letters);
+		while (buf[++i] != '\0')
+		{
+			nb += _putchar(buf[i]);
+		}
+		close(fd);
+		return (nb);
+	}
+
+	return (0);
+}
