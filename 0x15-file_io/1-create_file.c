432d2a4ba5f4d9b8b33fbeb78c7456c43b2094eb
iamprosper <thinkpadprosper@gmail.com>
2022-05-04 02:08:14 +0000

    Task 1 #2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if(text_content == NULL)
+	if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
+	}
 	if (fd != -1)
 	{
 		len_str = str_len(text_content);
