56f6cb569af792facbc09430fdcd185b8933f2a0
iamprosper <thinkpadprosper@gmail.com>
2022-05-04 01:44:22 +0000

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * create_file - A function that create a file
+ * @filename: The name of the file to create
+ * @text_content: The string to write or print to the file name
+ *
+ * Return: 1 in case of success, -1 in case of failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len_str;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd != -1)
+	{
+		len_str = str_len(text_content);
+		write(fd, text_content, len_str);
+		close(fd);
+		return (1);
+	}
+	return (-1);
+}
+/**
+ * str_len - Get the length of a string
+ * @string: The string
+ *
+ * Return: The length
+ */
+int str_len(char *string)
+{
+	int i = 0;
+
+	while (string[++i] != '\0')
+		;
+	return (i);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,4 +9,7 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
+int str_len(char *string);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 #endif
