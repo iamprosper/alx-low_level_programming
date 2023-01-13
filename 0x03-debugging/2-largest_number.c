0efd88b19207552afa30999658a9a8a141c893a6
iamprosper <thinkpadprosper@gmail.com>
2023-01-13 07:15:36 +0000

    Reviewing last equals number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,20 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a >= b)
 	{
-		if (b > c || a > c)
+		if (b >= c || a >= c)
 			largest = a;
 		else
 			largest = c;
 	}
-	else if (b > a)
-	{
-		if (a > c || b > c)
+	else if (a >= c || b >= c)
 			largest = b;
-		else
-			largest = c;
-	}
+	else
+		largest = c;
 
 	return (largest);
 }
