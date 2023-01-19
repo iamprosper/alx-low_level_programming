3daa3ff2cc06a3aceb519e8b6bd38bc3d392dffb
iamprosper <thinkpadprosper@gmail.com>
2023-01-19 14:04:32 +0000

    Task 11 print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,42 +1,54 @@
 #include "main.h"
-/**
- * print_to_98 - Prints normal digits from input number to 98
- * @n: int
- */
 
 /**
- * print_1_nb - Print the last digit of a number
+ * print_1_nb - Print nb form 10 to 99
  * @n: int
  */
+void print_1_nb(int n)
+{
+	_putchar(n % 10 + '0');
+}
 
 /**
  * print_10_nb - Print nb form 10 to 99
  * @n: int
  */
+void print_10_nb(int n)
+{
+	_putchar(n / 10 + '0');
+	print_1_nb(n);
+}
 
 /**
- * print_x_nb - Print any number
+ * print_100_nb - Print the last digit of a number
  * @n: int
  */
+void print_100_nb(int n)
+{
+	int nb;
 
-void print_1_nb(int n);
-void print_10_nb(int n);
-void print_100_nb(int n);
-void print_x_nb(int n);
+	nb = n % 100;
+	_putchar(n / 100 + '0');
+	print_10_nb(nb);
+}
 
+/**
+ * print_to_98 - Prints normal digits from input number to 98
+ * @n: int
+ */
 void print_to_98(int n)
 {
 	int i;
 
 	if (n < 98)
 	{
-		for(i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			int compute_nb;
 
 			if (i <= 9 && i >= 0)
 			{
-				if(i == n)
+				if (i == n)
 				{
 					print_1_nb(i);
 					_putchar(',');
@@ -133,27 +145,3 @@ void print_to_98(int n)
 	}
 }
 
-void print_x_nb(int x)
-{
-	_putchar(x + '0');
-}
-
-
-void print_100_nb(int n)
-{
-	int nb;
-	nb = n % 100;
-	_putchar(n / 100 + '0');
-	print_10_nb(nb);
-}
-
-void print_10_nb(int n)
-{
-	_putchar(n / 10 + '0');
-	print_1_nb(n);
-}
-
-void print_1_nb(int n)
-{
-	_putchar(n % 10 + '0');
-}
