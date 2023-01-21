56e9c4adf0e6e84d6e0c8f076e5ecab1f750aeef
iamprosper <thinkpadprosper@gmail.com>
2023-01-21 22:17:23 +0000

    Last task

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * fibo - Print 90 fibonacci numbers starting 1 and 2
+ * @n: integer, the total numbers
+ *
+ * Return: fb_number
+ */
+long int fibo(int n)
+{
+	if (n == 2)
+		return (1);
+	else if (n == 3)
+		return (2);
+	return (fibo(n - 1) + fibo(n - 2));
+}
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int prev = 1, next = 2;
+	long int temp;
+	int n = 90;
+	int i;
+
+	printf("%lu, ", prev);
+
+	for (i = 2; i <= n; i++)
+	{
+		if (i == n)
+			printf("%ld\n", next);
+		else
+			printf("%ld, ", next);
+
+		temp = next;
+		next += prev;
+		prev = temp;
+	}
+	return (0);
+}
