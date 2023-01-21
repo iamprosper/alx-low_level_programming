d82c5269552e15fb20a76384b28b8d860e2cca4f
iamprosper <thinkpadprosper@gmail.com>
2023-01-21 01:43:36 +0000

    Fib suit review#2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,6 +8,21 @@
  */
 int main(void)
 {
+	long int prev = 1, next = 2;
+	long int temp, sum = 0, limit = 4000000;
 
+	/*printf("%ld ,", prev);*/
+	while (next < limit)
+	{
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
+
+		temp = next;
+		next += prev;
+		prev = temp;
+	}
+	printf("%ld\n", sum);
 	return (0);
 }
