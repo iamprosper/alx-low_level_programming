8fc8a63b7edafc573542e88ac8d03a3982c274a5
iamprosper <thinkpadprosper@gmail.com>
2023-03-18 15:25:03 +0000

    Reviewing insert#4

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,31 +15,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new == NULL || index > idx)
 		return (NULL);
-
+	if (idx == 0)
+	{
+		new->n = n;
+		new->prev = NULL;
+		new->next = NULL;
+		*h = new;
+		return (new);
+	}
 	while (node != NULL)
 	{
 		if (idx == index)
 		{
 			new->n = n;
-
-			if (idx == 0)
-			{
-				new->prev = NULL;
-				new->next = NULL;
-				*h = new;
-			}
-			else
-			{
-				new->prev = node->prev;
-				new->next = node;
-				node->prev->next = new;
-				node->prev = new;
-			}
+			new->prev = node->prev;
+			new->next = node;
+			node->prev->next = new;
+			node->prev = new;
 
 			return (new);
 		}
 		node = node->next;
 		index++;
 	}
+
 	return (NULL);
 }
